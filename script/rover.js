a1465c565d9e07ee225657ada2b9e9e595b7ba37
Di <yaodi891024@gmail.com>
2016-10-02 22:16:44 -0500

    API move Rover add,  create necessary property and method. finish path move forward and backword.

--- script/rover.js
+++ script/rover.js
@@ -11,4 +11,62 @@ function rover(){
 	var obstacle = document.getElementById("obstacle").value;
 	var grid = [row, column];
 	var location = [startRow, startColumn];
+	path = path.split("");
+	var myRover = new moveRover(grid, location, startDir, path, obstacle);
+	console.log(myRover.paths(path));
+}
+
+function moveRover(grid, location, direction, path, obstacle){
+	self = this;
+	this.location = location;
+	this.direction = direction;
+	this.directions = ['N', 'E', 'S', 'W'];
+	this.grid = grid;
+	this.obstacle = obstacle;
+	this.paths = function(path){
+		for (var i = 0; i < path.length; i++){
+			if (path[i] === "f" || path[i] === "b"){
+				if (!straight(path[i])){
+					break;
+				}
+			} else if (path[i] === "l" || path[i] === "r"){
+				turn(path[i]);
+			}
+		}
+		wrap();
+		return this.location;
+	}
+
+	function straight(path){
+		var x = 0, y = 0;
+		if (self.direction === "N"){
+			x = 1;
+		} else if (self.direction === "S") {
+			x = -1;
+		} else if (self.direction === "E") {
+			y = 1;
+		} else if (self.direction === "W") {
+			y = -1;
+		}
+		var increase = (path === "f") ? [x, y] : [-x, -y];
+		var newLocation = [self.location[0] + increase[0], self.location[1] + increase[1]];
+		// if(isObstacle(newLocation)){
+		// 	return false;
+		// } 
+		self.location = newLocation;
+		return true;
+	}
+	function turn(){
+
+	}
+
+	function wrap(){
+		self.location[0] = (self.location[0] + self.grid[0]) % self.grid[0];
+		self.location[1] = (self.location[1] + self.grid[1]) % self.grid[1];
+	}
+
+	function isObstacle(newLocation){
+		
+	}
+
 }
