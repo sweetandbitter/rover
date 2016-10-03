d2923005fb587421ba66891c650b7c2ec5694475
Di <yaodi891024@gmail.com>
2016-10-02 22:39:45 -0500

    turn function  is add.

--- script/rover.js
+++ script/rover.js
@@ -46,7 +46,7 @@ function moveRover(grid, location, direction, path, obstacle){
 		} else if (self.direction === "E") {
 			y = 1;
 		} else if (self.direction === "W") {
-			y = -1;
+			y = - 1;
 		}
 		var increase = (path === "f") ? [x, y] : [-x, -y];
 		var newLocation = [self.location[0] + increase[0], self.location[1] + increase[1]];
@@ -56,7 +56,15 @@ function moveRover(grid, location, direction, path, obstacle){
 		self.location = newLocation;
 		return true;
 	}
-	function turn(){
+	function turn(path){
+		var l = self.directions.length;
+		var index = self.directions.indexOf(self.direction);  //index 0,1,2,3 means N,E,S,W
+		if(path === "l"){
+			index = (index - 1 + l) % l;
+		} else {
+			index = (index + 1) % l;
+		}
+		self.direction = self.directions[index];
 
 	}
 
@@ -66,7 +74,7 @@ function moveRover(grid, location, direction, path, obstacle){
 	}
 
 	function isObstacle(newLocation){
-		
+
 	}
 
 }
